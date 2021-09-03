938cb1fcc974b9f404721a70c23699a6b43a6e87
Yordi van Voorthuizen <yordi.vanvoorthuizen@itvitaelearning.nl>
2021-09-03 15:34:41 +0200

    Add project files.

---
+++ WeekOpdrachtSecurity.API/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeekOpdrachtSecurity.API.Models;
+using WeekOpdrachtSecurity.API.Services;
+
+namespace WeekOpdrachtSecurity.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ WeekOpdrachtSecurity.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WeekOpdrachtSecurity.Business
+{
+    public class Class1
+    {
+    }
+}
